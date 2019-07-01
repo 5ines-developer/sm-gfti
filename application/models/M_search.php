c383a7d8050a5386e24a28d1eb044974a922eb47
shahirdevp <shahirkm@5ine.in>
2019-07-01 13:24:27 +0530

    result page

---
+++ application/models/M_search.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_search extends CI_Model {
+
+    // get result()
+    public function getResult($query)
+    {
+        $this->db->from('product p');
+        $this->db->join('category c', 'c.id = p.category', 'left');
+        $this->db->like('title',$query)
+                ->or_like('tags',$query)
+                ->or_like('name', $query);
+        return $this->db->get()->result();
+    }
+
+    // get product with pagination
+    public function search_pagination($query,$perpage,$page)
+    {
+        $this->db->from('product p');
+        $this->db->limit($perpage, $page); 
+        $this->db->join('category c', 'c.id = p.category', 'left');
+        $this->db->like('title',$query)
+                ->or_like('tags',$query)
+                ->or_like('name', $query);
+        return $this->db->get()->result();
+    }
+
+    // get single product
+    public function single_product($id)
+    {
+        return $this->db->from('product p')
+        ->where('product_id', $id)
+        ->join('category c', 'c.id = p.category', 'right')
+        ->get()
+        ->row();
+    }
+
+    // brand select
+    public function brand_product($id = null)
+    {
+       return $this->db->where('product', $id)->get('brad_pricing')->result();
+    }
+
+}
+
+/* End of file M_search.php */
