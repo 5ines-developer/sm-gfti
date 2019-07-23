3f0ebdfdaaf54c97cdb67ab8dc6adc377f1381a5
shahirdevp <shahirkm@5ine.in>
2019-07-23 16:11:31 +0530

    orders on admin panel , place order and payment order

--- admin-panel/application/views/includes/sidebar.php
+++ admin-panel/application/views/includes/sidebar.php
@@ -37,7 +37,14 @@
                       </ul>
                   </li>
                 <li><a href="<?php echo base_url() ?>manage-employee"><i class="fa fa-users" aria-hidden="true"></i>Manage Employee</a></li>
-                <li><a href="<?php echo base_url() ?>orders"><i class="fa fa-cart-plus" aria-hidden="true"></i></i>orders</a></li>
+                <li>
+                    <a > <i class="fa fa-cart-plus" aria-hidden="true"></i>orders <span class="fa fa-chevron-down"></span> </a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?php echo base_url() ?>orders">All Orders</a></li>
+                      <li><a href="<?php echo base_url() ?>orders?q=payment-orders&scode=1">Payment Orders</a></li>
+                      <li><a href="<?php echo base_url() ?>orders?q=place-orders&scode=2">Place orders</a></li>
+                    </ul>
+                </li>
                 <li><a><i class="fa fa-newspaper-o" aria-hidden="true"></i> Billing Address<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="<?php echo base_url() ?>add-billing-address">Add Billing Address</a></li>
