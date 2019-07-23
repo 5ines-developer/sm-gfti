f458f95e25c8fd7a2ad48b56eb477c1e66416b15
shahirdevp <shahirkm@5ine.in>
2019-07-23 16:42:29 +0530

    cart count

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -353,6 +353,11 @@ class Cart extends CI_Controller {
             
         }
 
+        // cart cout
+        public function cartcount()
+        {
+            echo $this->m_cart->cart_item($this->session->userdata('sid'));
+        }
 
         
 
